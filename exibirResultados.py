3b36cab3bebb773fa34112da6ab43e2168ed0964
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-06-21 14:54:22 -0300

    Silanciando os Warnings do TF Mudando pequenos detalhes em alguns parametros resolvendo erros de tipos

--- exibirResultados.py
+++ exibirResultados.py
@@ -6,31 +6,26 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from io import BytesIO
 from PIL import Image
-from tensorflow.image import ssim
+from tensorflow._api.v2.image import ssim
 from scipy.ndimage import gaussian_filter
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from DataMod import NoiseGen
 
+import logging
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
-import warnings
-
-warnings.filterwarnings('ignore')
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 def Ssim (i1, i2):
-    return -ssim(i1, i2, max_val=255, filter_size = 7)
+    return -ssim(i1, i2, max_val=255, filter_size = 11)
 
 
 (xTrain, yTrain),(xTest, yTest) = tf.keras.datasets.cifar10.load_data()
 
 newXtest = tf.image.rgb_to_grayscale(xTest)
 newXtest = np.array(newXtest)
-
-noiseDataTest = NoiseGen()
-noiseDataTest.grayLowNoiseMkr(newXtest, 19)
-
 
 del xTrain, xTest, yTrain, yTest
 
@@ -51,10 +46,11 @@ newTestBase = np.array(newTestBase)
 newTestBase = newTestBase.reshape(10000,32,32,1)
 
 noiseDataTest = NoiseGen()
-noiseDataTest.grayLowNoiseMkr(newTestBase, 19)
+noiseDataTest.grayLowNoiseMkr(newTestBase, 10)
 
+noiseDataTest.dataSet = noiseDataTest.dataSet
 
-json_file = open('model5.json', 'r')
+json_file = open('Unet.json', 'r')
 
 loaded_json_file = json_file.read()
 
@@ -62,18 +58,19 @@ json_file.close()
 
 nNet = kr.models.model_from_json(loaded_json_file)
 
-checkpoint_path = "checkpoints5/cp.ckpt"
+checkpoint_path = "checkpoints6/cp.ckpt"
 
-nNet.load_weights(checkpoint_path)
+nNet.load_weights(checkpoint_path).expect_partial()
 
-imgs1 = nNet.predict(newTestBase)
+imgs1 = nNet.predict(noiseDataTest.dataSet)
 
-imgs1 = imgs1.astype('uint8')
+imgs1 = (np.clip(imgs1, 0 , 255)).astype('uint8')
+
+
 
 plt.figure(figsize=(20,4))
 
-
-gaussImgs = gaussian_filter(noiseDataTest.dataSet, 0.45)
+gaussImgs = gaussian_filter(noiseDataTest.dataSet, 0.38)
 
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
@@ -90,7 +87,7 @@ plt.figure(figsize=(20,4))
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
   plt.imshow(gaussImgs[2*i+1050].reshape(32,32))
-  plt.xlabel(-1*Ssim(gaussImgs[2*i+1050], newXtest[2*i+1050]).numpy())
+  plt.xlabel(round(-1*Ssim(gaussImgs[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(),2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
@@ -102,7 +99,7 @@ plt.figure(figsize=(20,4))
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
   plt.imshow(noiseDataTest.dataSet[2*i+1050].reshape(32,32))
-  plt.xlabel(-1*Ssim(noiseDataTest.dataSet[2*i+1050], newXtest[2*i+1050]).numpy())
+  plt.xlabel(round(-1*Ssim(noiseDataTest.dataSet[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(),2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
@@ -113,7 +110,7 @@ plt.figure(figsize=(20,4))
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
   plt.imshow(imgs1[2*i+1050].reshape(32,32))
-  plt.xlabel(-1*Ssim(imgs1[2*i+1050], newXtest[2*i+1050]).numpy())
+  plt.xlabel(round(-1*Ssim(imgs1[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(), 2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
