cc2d09ce43b19a88e6b1b3b4d2027b0c0534f3ef
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-06-21 15:13:46 -0300

    Adicionando Relu em todas as camadas, havia esquecido

--- Unet.py
+++ Unet.py
@@ -13,39 +13,39 @@ Llayer1_3 = kr.layers.Conv2D( filters= 4, kernel_size=(4,4), padding='same' )(Ll
 
 #Left second block:
 Llayer2_1 = kr.layers.MaxPool2D()(Llayer1_3)
-Llayer2_2 = kr.layers.Conv2D( filters= 8, kernel_size=(4,4), padding='same' )(Llayer2_1)
-Llayer2_3 = kr.layers.Conv2D( filters= 16, kernel_size=(4,4), padding='same' )(Llayer2_2)
+Llayer2_2 = kr.layers.Conv2D( filters= 8, kernel_size=(4,4), activation='relu' , padding='same' )(Llayer2_1)
+Llayer2_3 = kr.layers.Conv2D( filters= 16, kernel_size=(4,4), activation='relu' , padding='same' )(Llayer2_2)
 
 #Left thrid block:
 Llayer3_1 = kr.layers.MaxPool2D()(Llayer2_3)
-Llayer3_2 = kr.layers.Conv2D( filters= 32, kernel_size=(4,4), padding='same' )(Llayer3_1)
-Llayer3_3 = kr.layers.Conv2D( filters= 64, kernel_size=(4,4), padding='same'  )(Llayer3_2)
+Llayer3_2 = kr.layers.Conv2D( filters= 32, kernel_size=(4,4), activation='relu' , padding='same' )(Llayer3_1)
+Llayer3_3 = kr.layers.Conv2D( filters= 64, kernel_size=(4,4), activation='relu' , padding='same'  )(Llayer3_2)
 
 #Left fourth block:
 Llayer4_1 = kr.layers.MaxPool2D()(Llayer3_3)
-Llayer4_2 = kr.layers.Conv2D( filters= 128, kernel_size=(4,4), padding='same'  )(Llayer4_1)
-Llayer4_3 = kr.layers.Conv2D( filters= 256, kernel_size=(4,4), padding='same'  )(Llayer4_2)
+Llayer4_2 = kr.layers.Conv2D( filters= 128, kernel_size=(4,4), activation='relu' , padding='same'  )(Llayer4_1)
+Llayer4_3 = kr.layers.Conv2D( filters= 256, kernel_size=(4,4), activation='relu' , padding='same'  )(Llayer4_2)
 
 #Right thrid block:
 Rlayer3_1_2 = kr.layers.UpSampling2D()(Llayer4_3)
 Rlayer3_1 = kr.layers.concatenate([Llayer3_3, Rlayer3_1_2], axis=-1)
-Rlayer3_2 = kr.layers.Conv2D( filters= (64+256)//2, kernel_size=(4,4), padding='same'  )(Rlayer3_1)
-Rlayer3_3 = kr.layers.Conv2D( filters=  80, kernel_size=(4,4), padding='same'  )(Rlayer3_2)
+Rlayer3_2 = kr.layers.Conv2D( filters= (64+256)//2, kernel_size=(4,4), activation='relu' , padding='same'  )(Rlayer3_1)
+Rlayer3_3 = kr.layers.Conv2D( filters=  80, kernel_size=(4,4), activation='relu' , padding='same'  )(Rlayer3_2)
 
 #right second block:
 Rlayer2_1_2 = kr.layers.UpSampling2D()(Llayer3_3)
 Rlayer2_1 = kr.layers.concatenate([Llayer2_3, Rlayer2_1_2], axis=-1)
-Rlayer2_2 = kr.layers.Conv2D( filters= (16+80)//2, kernel_size=(4,4), padding='same' )(Rlayer2_1)
-Rlayer2_3 = kr.layers.Conv2D( filters= 24, kernel_size=(4,4), padding='same' )(Rlayer2_2)
+Rlayer2_2 = kr.layers.Conv2D( filters= (16+80)//2, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer2_1)
+Rlayer2_3 = kr.layers.Conv2D( filters= 24, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer2_2)
 
 #right frist block:
 Rlayer1_1_2 = kr.layers.UpSampling2D()(Llayer2_3)
 Rlayer1_1 = kr.layers.concatenate([Llayer1_3, Rlayer1_1_2], axis=-1)
-Rlayer1_2 = kr.layers.Conv2D( filters= (24+4)//2, kernel_size=(4,4), padding='same' )(Rlayer1_1)
-Rlayer1_3 = kr.layers.Conv2D( filters= 7, kernel_size=(4,4), padding='same' )(Rlayer1_2)
-Rlayer1_4 = kr.layers.Conv2D( filters= 4, kernel_size=(4,4), padding='same' )(Rlayer1_3)
-Rlayer1_5 = kr.layers.Conv2D( filters= 2, kernel_size=(4,4), padding='same' )(Rlayer1_4)
-Rlayer1_6 = kr.layers.Conv2D( filters= 1, kernel_size=(4,4), padding='same' )(Rlayer1_5)
+Rlayer1_2 = kr.layers.Conv2D( filters= (24+4)//2, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer1_1)
+Rlayer1_3 = kr.layers.Conv2D( filters= 7, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer1_2)
+Rlayer1_4 = kr.layers.Conv2D( filters= 4, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer1_3)
+Rlayer1_5 = kr.layers.Conv2D( filters= 2, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer1_4)
+Rlayer1_6 = kr.layers.Conv2D( filters= 1, kernel_size=(4,4), activation='relu' , padding='same' )(Rlayer1_5)
 
 model = kr.models.Model(inputs=inputs, outputs=Rlayer1_6, name="Unet")
 
