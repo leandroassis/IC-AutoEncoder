02e1098a18d0610f59a5fc222bd2e2e1245d0ddb
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-07-05 17:03:21 -0300

    Modificando o código para se adequar ao DataMod novo

--- exibirResultados.py
+++ exibirResultados.py
@@ -9,7 +9,7 @@ from PIL import Image
 from tensorflow._api.v2.image import ssim
 from scipy.ndimage import gaussian_filter
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from DataMod import NoiseGen
+from DataMod import DataMod
 
 import logging
 import os
@@ -22,34 +22,17 @@ def Ssim (i1, i2):
     return -ssim(i1, i2, max_val=255, filter_size = 11)
 
 
+#gerando a base
 (xTrain, yTrain),(xTest, yTest) = tf.keras.datasets.cifar10.load_data()
 
-newXtest = tf.image.rgb_to_grayscale(xTest)
-newXtest = np.array(newXtest)
+test = DataMod(xTest)
+test.rbg_to_gray()
 
-del xTrain, xTest, yTrain, yTest
+test_target = test.dataSet.copy()
+test.add_jpeg_compression_to_grayscale(compress_quality=30)
+test.add_standard_Noise(max_pixel_var=10)
 
-newTestBase = []
-
-for indice in range(newXtest.shape[0]):
-  buffer = BytesIO()
-  img = Image.fromarray(newXtest[indice].reshape(32,32), mode="L")
-  img.save(buffer, "JPEG", quality=30)
-  image = Image.open(buffer)
-  image = np.asarray(image)
-  newTestBase.append(image)
-  buffer.close()
-
-del buffer
-
-newTestBase = np.array(newTestBase)
-newTestBase = newTestBase.reshape(10000,32,32,1)
-
-noiseDataTest = NoiseGen()
-noiseDataTest.grayLowNoiseMkr(newTestBase, 10)
-
-noiseDataTest.dataSet = noiseDataTest.dataSet
-
+#Load do modelo
 json_file = open('Unet.json', 'r')
 
 loaded_json_file = json_file.read()
@@ -62,7 +45,9 @@ checkpoint_path = "checkpoints6/cp.ckpt"
 
 nNet.load_weights(checkpoint_path).expect_partial()
 
-imgs1 = nNet.predict(noiseDataTest.dataSet)
+
+# predict
+imgs1 = nNet.predict(test.dataSet)
 
 imgs1 = (np.clip(imgs1, 0 , 255)).astype('uint8')
 
@@ -70,16 +55,16 @@ imgs1 = (np.clip(imgs1, 0 , 255)).astype('uint8')
 
 plt.figure(figsize=(20,4))
 
-gaussImgs = gaussian_filter(noiseDataTest.dataSet, 0.38)
+gaussImgs = gaussian_filter(test.dataSet, 0.38)
 
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
-  plt.imshow(newXtest[2*i+1050].reshape(32,32))
+  plt.imshow(test_target[2*i+1050].reshape(32,32))
   plt.gray()
   ax.get_xaxis().set_visible(False)
   ax.get_yaxis().set_visible(False)
 
-plt.show()
+plt.savefig("img0.png")
 
 
 plt.figure(figsize=(20,4))
@@ -87,32 +72,32 @@ plt.figure(figsize=(20,4))
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
   plt.imshow(gaussImgs[2*i+1050].reshape(32,32))
-  plt.xlabel(round(-1*Ssim(gaussImgs[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(),2))
+  plt.xlabel(round(-1*Ssim(gaussImgs[2*i+1050], test_target[2*i+1050].astype('uint8')).numpy(),2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
-plt.show()
+plt.savefig("img1")
 
 
 plt.figure(figsize=(20,4))
 
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
-  plt.imshow(noiseDataTest.dataSet[2*i+1050].reshape(32,32))
-  plt.xlabel(round(-1*Ssim(noiseDataTest.dataSet[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(),2))
+  plt.imshow(test.dataSet[2*i+1050].reshape(32,32))
+  plt.xlabel(round(-1*Ssim(test.dataSet[2*i+1050], test_target[2*i+1050].astype('uint8')).numpy(),2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
-plt.show()
+plt.savefig("img2")
 
 plt.figure(figsize=(20,4))
 
 for i in range (10):
   ax = plt.subplot(2,10, i+1)
   plt.imshow(imgs1[2*i+1050].reshape(32,32))
-  plt.xlabel(round(-1*Ssim(imgs1[2*i+1050], newXtest[2*i+1050].astype('uint8')).numpy(), 2))
+  plt.xlabel(round(-1*Ssim(imgs1[2*i+1050], test_target[2*i+1050].astype('uint8')).numpy(), 2))
   plt.gray()
   ax.get_yaxis().set_visible(False)
 
-plt.show()
+plt.savefig("img3")
 
