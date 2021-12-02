ef9253d2db7d805eace52ab1d80b4b8174229a3b
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-12-01 23:32:34 -0300

    Novas funcionalidades do genrenciador de diretórios

--- modules/DirManager.py
+++ modules/DirManager.py
@@ -1,9 +1,8 @@
 from abc import ABC, abstractmethod
 from genericpath import isdir
-from os import makedirs, removedirs
-
+from os import makedirs
+import shutil
 from tensorflow.python.lib.io.file_io import file_exists
-
 
 
 class DirManagerABC (ABC):
@@ -32,10 +31,12 @@ class KerasDirManager (DirManagerABC):
                 ) -> None:
 
         self.logs_dir = f"logs/{dataset_name}/{loss_name}/{model_name}/{training_idx}"
+        
         self.model_save_pathname = f"logs/{dataset_name}/{loss_name}/{model_name}/{training_idx}/model"
         self.csv_pathname = f"logs/{dataset_name}/{loss_name}/{model_name}/{training_idx}/CsvLoger.csv"
-
-        self.last_model_save_pathname = f'logs/last_model_save'
+        
+        self.last_model_save_pathname = f'logs/last_model_saved'
+        self.models_saved_pathname = f'logs/models_saved_pathname'
         self.last_model_save = self._load_last_model_pathname()
         
     def make_all_dirs(self) -> None:
@@ -45,8 +46,8 @@ class KerasDirManager (DirManagerABC):
 
     def remove_last_save(self):
 
-        if self.last_model_save:
-            removedirs(self.last_model_save)
+        if self.last_model_save and isdir(self.last_model_save):
+            shutil.rmtree(self.last_model_save)
         
     def _load_last_model_pathname(self) -> str:
 
@@ -55,9 +56,19 @@ class KerasDirManager (DirManagerABC):
                 path = file.read()
                 file.close()
 
-            return path
+                return path
 
     def save_actual_model_pathname_as_last(self) -> None:
-
         with open(f'logs/last_model_saved', 'w') as file:
             file.write(self.model_save_pathname)
+
+
+    def _save_pathname_in_the_list() -> None:
+        pass
+
+
+    def get_logs_size(self):
+        pass
+
+    def get_actual_training_logs_size(self):
+        pass
