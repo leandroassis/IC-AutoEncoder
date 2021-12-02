64e70824be4eea56c869e20005e5de8139b01f80
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-12-01 23:24:47 -0300

    Novos metodos do tensorboard writer

--- modules/TensorBoardWriter.py
+++ modules/TensorBoardWriter.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+from os import write
+from pandas.core.frame import DataFrame
+from tensorflow.python.keras.engine.training import Model
+from tensorflow.python.ops import summary_ops_v2
 import tensorflow as tf
-
-
 
 
 class TensorBoardWriterABC (ABC):
@@ -28,17 +30,31 @@ class TensorBoardWriter (TensorBoardWriterABC):
         """
 
         """
-
         writer = tf.summary.create_file_writer(self.file_path)
         with writer.as_default():
             tf.summary.image(name, images, step=0, max_outputs= 4)
-            writer.flush()
+        writer.close()
 
-    def write_scalars (self, scalars) -> None:
+
+    def write_net_graph (self, model: Model):
+
+        writer = tf.summary.create_file_writer(self.file_path)
+        with writer.as_default():
+            summary_ops_v2.keras_model('keras', model, step=0)
+        writer.close()
+
+
+    def write_scalars (self, scalars: DataFrame) -> None:
         """
-            
+            scalars:
+                A Dataframe with epoch loss and metrics
         """
-        pass
+
+        writer = tf.summary.create_file_writer(self.file_path)
+        with writer.as_default():
+            tf.summary.scalar()
+        writer.close()
+        
 
 
     
