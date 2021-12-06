bc90bb4861acbede4d9ad403da34d7c23910b01d
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2021-12-05 23:03:49 -0300

    nova implementação da classe

--- modules/NeuralNetData.py
+++ modules/NeuralNetData.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod, abstractproperty
 from dataclasses import dataclass
 from typing import Any
 from tensorflow.keras.models import Model, model_from_json
-import json
-from misc import get_neural_net_node_deep
+from KerasModelInfo import *
+
 
 
 @dataclass
@@ -80,33 +80,30 @@ class KerasNeuralNetData(NeuralNetDataABC):
 
         self.total_layers: int = self.model.layers.__len__()
 
-        self.total_treinable_layers: int = self._get_number_of_treinable_layers()
+        self.total_treinable_layers: int = get_number_of_layers_with_params(self.model)
 
-        self.total_regularizers: dict = self._get_number_of_regularizers()
+        self.total_regularizers: dict = get_number_of_regularizers(self.model)
         """A `dict` containing the total number of regularizers of each type"""
 
-        self.regularizer_map: dict = self._get_regularizer_map()
+        self.regularizer_map: dict = get_regularizer_distribution(self.model)
         """A `dict` describing the distribution of regularizers"""
         
-        self.regularizer_L1_map: dict = self._get_regularizer_map("L1")
+        self.regularizer_L1_map: dict = get_regularizer_distribution(self.model, "L1")
         """A `dict` describing the distribution of regularizers of type L1"""
 
-        self.regularizer_L2_map: dict = self._get_regularizer_map("L2")
+        self.regularizer_L2_map: dict = get_regularizer_distribution(self.model, "L2")
         """A `dict` describing the distribution of regularizers of type L2"""
 
-        self.regularizer_L3_map: dict = self._get_regularizer_map("L3")
-        """A `dict` describing the distribution of regularizers of type L2"""
-
-        self.parameters_distribution: int = self._get_parameters_distribution()
+        self.parameters_distribution: int = get_parameters_distribution(self.model)
         """A `int` that describes how the parameters are distributed along a neural net"""
 
-        self.initializers_count: dict = self._get_initializer_info()
+        self.initializers_count: dict = get_initializer_info(self.model)
         """A `dict` containing initializers count of each type."""
 
-        self.number_segments: int = self._get_paralelism_info()
+        self.number_segments: int = get_paralelism_info(self.model)
         """The number of paralel segments in the neural network"""
 
-        dropout_count, dropout_distribution = self._get_layer_distribution_and_count("Dropout")
+        dropout_count, dropout_distribution = get_layer_distribution_and_count(self.model, "Dropout")
 
         self.dropout_count: int = dropout_count
         """ The number of dropout layers in the network """
@@ -114,7 +111,7 @@ class KerasNeuralNetData(NeuralNetDataABC):
         self.dropout_distribution: float = dropout_distribution
         """ The distribution of dropout layers in the network """
 
-        batch_normalization_count, batch_normalization_distribution = self._get_layer_distribution_and_count("BatchNormalization")
+        batch_normalization_count, batch_normalization_distribution = get_layer_distribution_and_count(self.model ,"BatchNormalization")
 
         self.batch_normalization_count: int = batch_normalization_count
         """ The number of batch normalization layers in the network """
@@ -123,185 +120,6 @@ class KerasNeuralNetData(NeuralNetDataABC):
         """ The distribution of batch normalization layers in the network """
 
         super().__init__()
-
-
-    def _get_number_of_regularizers(self) -> dict:
-        
-        layers_config: dict = json.loads(self.model.to_json())['config']['layers']
-
-        number_of_regularizers: dict = {
-            "kernel" : 0,
-            "bias" : 0,
-            "activity" : 0
-        }
-
-        for layer in layers_config:
-            
-            try:
-                if (layer['config']['kernel_regularizer'] != None):
-                    number_of_regularizers["kernel"] += 1
-                
-                if layer['config']['bias_regularizer'] != None:
-                    number_of_regularizers["bias"] += 1
-                
-                if layer['config']['activity_regularizer'] != None:
-                    number_of_regularizers["activity"] += 1
-
-            except KeyError:
-                pass # The layer dont contain a regularizer, Ex: MaxPoll
-
-        return number_of_regularizers
-
-
-    def _get_regularizer_map(self, type: str = "") -> dict:
-
-        layers_config: dict = json.loads(self.model.to_json())['config']['layers']
-
-        distribution_of_regularizers: dict = {
-            "kernel" : 0.0,
-            "bias" : 0.0,
-            "activity" : 0.0
-        }
-
-        nodes_depp = get_neural_net_node_deep(self.model)
-
-
-        for layer in layers_config:
-
-            if 'kernel_regularizer' in layer['config']:
-
-                if (layer['config']['kernel_regularizer'] != None):
-                    
-                    if type == "":
-                        distribution_of_regularizers["kernel"] += nodes_depp[layer['name']]
-                    
-                    elif layer['config']['kernel_regularizer']['class_name'] == type:
-                        distribution_of_regularizers["kernel"] += nodes_depp[layer['name']]
-            
-            if 'bias_regularizer' in layer['config']:
-
-                if (layer['config']['bias_regularizer'] != None):
-                    
-                    if type == "":
-                        distribution_of_regularizers["bias"] += nodes_depp[layer['name']]
-                    
-                    elif layer['config']['bias_regularizer']['class_name'] == type:
-                        distribution_of_regularizers["bias"] += nodes_depp[layer['name']]
-            
-            
-            if 'activity_regularizer' in layer['config']:
-
-                if (layer['config']['activity_regularizer'] != None):
-                    
-                    if type == "":
-                        distribution_of_regularizers["activity"] += nodes_depp[layer['name']]
-            
-                    elif layer['config']['activity_regularizer']['class_name'] == type:
-                        distribution_of_regularizers["activity"] += nodes_depp[layer['name']]
-            
-        max_depp = (max(nodes_depp.values()))
-
-        if self.total_regularizers['activity'] > 0:
-            distribution_of_regularizers['activity'] = distribution_of_regularizers['activity']/( (max_depp)*(self.total_regularizers['activity']))
-        
-        if self.total_regularizers['bias'] > 0: 
-            distribution_of_regularizers['bias'] = distribution_of_regularizers['bias']/( (max_depp)*(self.total_regularizers['bias']))
-
-        if self.total_regularizers['kernel'] > 0:
-            distribution_of_regularizers['kernel'] = distribution_of_regularizers['kernel']/( (max_depp)*(self.total_regularizers['kernel']) )
-
-        return distribution_of_regularizers
-
-
-    def _get_paralelism_info(self):
-        """
-            Get the number of paralel sequences of neurons, with the number of inbound nodes of the layers.
-        """
-
-        info: dict = {
-            'number' : 1
-        }
-
-        layers_config: dict = json.loads(self.model.to_json())['config']['layers']
-        
-        for layer in layers_config:
-
-            if layer['class_name'] == "Concatenate":
-                info['number'] += layer['inbound_nodes'][0].__len__() - 1
-
-            
-        return info['number']
-
-
-    def _get_parameters_distribution(self) -> int:
-
-        nodes_depp = get_neural_net_node_deep(self.model)
-
-        distribution: float = 0.0
-        
-        for layer in self.model.layers:
-
-            distribution += layer.count_params()*nodes_depp[layer.name] 
-            
-        distribution = distribution/(self.total_params*self.total_layers) # Normalization
-            
-        return distribution
-
-
-
-    def _get_initializer_info(self) -> dict:
-        """
-            Get the intitializer type count for bias and kernel
-        """
-
-        count_of_initializers: dict = {
-            "kernel" : {},
-            "bias" : {},
-            }
-
-        kernel_initializer_type: dict = {
-        }
-
-        bias_initializer_type: dict = {
-        }
-
-        layers_config: dict = json.loads(self.model.to_json())['config']['layers']
-
-        for layer in layers_config:
-
-            if 'kernel_initializer' in layer['config']:
-                kernel_initializer = layer['config']['kernel_initializer']['class_name']
-
-                if kernel_initializer in kernel_initializer_type:
-                    kernel_initializer_type[kernel_initializer] += 1
-                else:
-                    kernel_initializer_type[kernel_initializer] = 1
-
-            if 'bias_initializer' in layer['config']:
-                bias_initializer = layer['config']['bias_initializer']['class_name']
-
-                if bias_initializer in bias_initializer_type:
-                    bias_initializer_type[bias_initializer] += 1
-                else:
-                    bias_initializer_type[bias_initializer] = 1
-        
-        count_of_initializers['kernel'] = kernel_initializer_type
-        count_of_initializers['bias'] = bias_initializer_type
-
-        return count_of_initializers
-
-
-    def _get_number_of_treinable_layers(self) -> int:
-
-        total_layers = 0
-
-        for layer in self.model.layers:
-
-            if layer.count_params() > 0:
-
-                total_layers += 1
-                
-        return total_layers
 
 
     def get_csv_data(self) -> dict:
@@ -340,27 +158,6 @@ class KerasNeuralNetData(NeuralNetDataABC):
         }
 
         return data
-
-    def _get_layer_distribution_and_count (self, layer_class_name) -> tuple:
-
-        layers_config: dict = json.loads(self.model.to_json())['config']['layers']
-
-        nodes_depp: dict = get_neural_net_node_deep(self.model)
-
-        count: int = 0
-
-        distribution: float = 0
-
-        for layer in layers_config:
-
-            if layer['class_name'] == layer_class_name:
-                count += 1
-                distribution += nodes_depp[layer['name']]
-
-        if count:
-            distribution = distribution/(count*max(nodes_depp.values()))
-
-        return count, distribution
 
     
     def get_model(self) -> Model:
