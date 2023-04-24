722b77712675f15dfa193babab5de70c23a885d5
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2023-04-23 22:14:07 -0300

    new data and removing old files

--- ResidualAutoencoder.py
+++ ResidualAutoencoder.py
@@ -2,14 +2,14 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Add , In
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
 
-model_name = "ResidualAutoEncoder-0.0-64x64"
+model_name = "ResidualAutoEncoder-0.1-64x64"
 
 inputs = Input(shape=(64,64,1))
 layer_1 = Conv2D(filters = 1, kernel_size = 4, padding = 'same', activation = 'relu')(inputs)
 layer_2 = Add()([layer_1, inputs])
-layer_3 = Conv2D(filters = 10, kernel_size = 4, padding = 'same', activation = 'relu')(layer_2)
-layer_4 = Conv2D(filters = 20, kernel_size = 4, padding = 'same', activation = 'relu')(layer_3)
-layer_5 = Conv2D(filters = 10, kernel_size = 4, padding = 'same', activation = 'relu')(layer_4)
+layer_3 = Conv2D(filters = 20, kernel_size = 4, padding = 'same', activation = 'relu')(layer_2)
+layer_4 = Conv2D(filters = 50, kernel_size = 4, padding = 'same', activation = 'relu')(layer_3)
+layer_5 = Conv2D(filters = 20, kernel_size = 4, padding = 'same', activation = 'relu')(layer_4)
 layer_6 = Add()([layer_3, layer_5])
 layer_7 = Conv2D(filters = 1, kernel_size = 4, padding = 'same', activation = 'relu')(layer_6)
 
