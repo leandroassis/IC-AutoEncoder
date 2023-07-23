e16900dbe1cbf33bfa2241ec6efe29a56ed2374a
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-07-23 17:34:45 -0300

    autoencoder com tiny

--- run1.py
+++ run1.py
@@ -2,28 +2,29 @@ from sys import path
 from os import getcwd, environ
 
 path.insert(0, getcwd()) # adding IC-AutoEncoder folder to path
-path.insert(0, getcwd() + '/modules/') # adding modules folder to path
+
 from modules.DataMod import DataSet
 from modules.CustomLosses import LSSIM
 from modules.misc import ssim_metric, psnrb_metric
 from tensorflow.keras.optimizers import Adam
 
-import mlflow.tensorflow
+import mlflow.keras
 
+from models.autoEncoder import autoEncoder
 
 environ["CUDA_VISIBLE_DEVICES"] = "1"
 
-
-mlflow.tensorflow.autolog()
 
 tinyDataSet, cifarDataSet, cifarAndTinyDataSet = DataSet(), DataSet(), DataSet()
 tinyDataSet = tinyDataSet.load_rafael_tinyImagenet_64x64_noise_data()
 cifarDataSet = cifarDataSet.load_rafael_cifar_10_noise_data()
 cifarAndTinyDataSet = cifarAndTinyDataSet.concatenateDataSets(cifarDataSet, tinyDataSet)
 
-autoEncoder.compile(optimizer = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7, amsgrad=False), loss = LSSIM(), metrics = [ssim_metric, psrnb_metric])
+autoEncoder.compile(optimizer = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7, amsgrad=False), loss = LSSIM(), metrics = [ssim_metric, psnrb_metric])
 
-autoEncoder.fit(
+mlflow.keras.autolog()
+
+history = autoEncoder.fit(
         x = tinyDataSet.x_train,
         y = tinyDataSet.y_train,
         batch_size = 20,
@@ -42,6 +43,6 @@ autoEncoder.fit(
         use_multiprocessing = False
 )
 
-autoEncoder.evaluate()
+score = autoEncoder.evaluate(tinyDataSet.x_test, tinyDataSet.y_test, batch_size = 20, verbose = 1)
 
-autoEncoder.save_weights("nNet_models/" + model_name + ".h5")
+autoEncoder.save_weights("models/weights" + autoEncoder.model_name + ".h5")
