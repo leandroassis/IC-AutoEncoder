986909a90ac8775f04782c0af28cc3812ff8acd5
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-08-27 13:59:29 -0300

    testando ruido aditivo

---
+++ src/run2/test.py
@@ -0,0 +1,17 @@
+from sys import path
+from os import getcwd, environ, walk
+
+path.insert(0, getcwd())
+path.insert(0, getcwd() + "/modules/")
+environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+from modules.DataMod import DataSet
+
+tinyDataSet = DataSet()
+print(tinyDataSet.x_test[15])
+
+tinyDataSet.load_rafael_tinyImagenet_64x64_noise_data()
+tinyDataSet = tinyDataSet.add_gaussian_noise()
+
+print(tinyDataSet.x_test[15])
+
